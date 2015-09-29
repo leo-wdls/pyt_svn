d595be450d66eeeae1602db00bcc3f9cdd771d82
leo.wang <leo.wdls@gmail.com>
2015-09-29 17:05:03 +0800

    Create pyt_svn.py
    init python svn command

---
+++ pyt_svn.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# FileName: pyt_svn.pyt
+# Description: svn command used python language
+
+import sys
+import subprocess
+
+repo_url_list = {"1":"https://svn.jetlive.net:8443/svn/rd/doc/芒果TV全志定制芯片",
+                 "2":"https://svn.jetlive.net:8443/svn/rd/code/广西广电网络机顶盒中间件平台项目/trunk"}
+
+class repo_list:
+    def set_repo_list(self, list):
+        self.repo_list = list
+
+    def get_repo_list(self):
+        return self.repo_list
+
+    def show_repo_list(self):
+        i = 1
+        for k, v in self.repo_list.items():
+            print "repo url number[%d]:" %i
+            print v
+            print
+            i = i+1
+            
+
+def help():
+    print "pyt_svn repo_url"
+
+# main
+if __name__ == "__main__":
+    repo_lists = repo_list()
+    repo_lists.set_repo_list(repo_url_list)
+    
+    if (len(sys.argv)==1):
+        repo_lists.show_repo_list()
+        url_num = int(raw_input("Enter url number:  "))
+        url = repo_lists.get_repo_list()[str(url_num)]
+        cmd = "svn co "+str(url)
+
+    elif (len(sys.argv)==2):
+        cmd = "svn co "+sys.argv[1]
+
+    elif (len(sys.argv)==3):
+        cmd = "svn co "+sys.argv[1]+sys.argv[2]
+
+    # print cmd
+    child = subprocess.Popen(cmd, shell=True)
+    child.wait()
+    print "Down OK!"
+
+    
+        
+
+    
